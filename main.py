22d0a3db9960e835e55039873ef916a407c0bf58
Mohammad Behzadi <105620732+MohammadBeh@users.noreply.github.com>
2022-07-29 23:27:14 -0400

    Update main.py

--- main.py
+++ main.py
@@ -3,12 +3,12 @@ from config import *
 from trainer import * 
 from utils import * 
 from tensorflow.keras.models import load_model
-
+from prediction import *
 cfg = ConfigGAN()
 
 if cfg.traintest == 'traintest':
     
-    dataset = load_real_samples(cfg.data_root, test = False)
+    dataset = load_real_samples(cfg, train = True)
     print('Dataset is loaded')
     d_model = Discriminator()
     g_model = Generator(cfg)
@@ -18,20 +18,15 @@ if cfg.traintest == 'traintest':
     model = train(cfg,d_model, g_model, gan_model, dataset)
 
 
-    # test_dataset = load_real_samples(cfg.data_root, test = True)
-    # lat_input = generate_latent_points(100, test_dataset[0].shape[0])
-    # print('Test Data is loaded', test_dataset[0].shape, test_dataset[1].shape)
-    # ypred = model.predict([test_dataset[0],lat_input])
-    
-    # save_prediction(cfg.n_testsamples, cfg.pred_dir, test_dataset[1],ypred)
-    # print('The predicted images has been saved!')
+    test_dataset = load_real_samples(cfg, train  = False)
+    Accuracy, Recall, L2 = Predict(cfg,model, test_dataset)
     
     
-#if cfg.traintest == 'test':
- #   test_dataset = load_real_samples(cfg.data_root, test = True)
- #   lat_input = generate_latent_points(100, test_dataset[0].shape[0])
- #   print('Test Data is loaded', test_dataset[0].shape, test_dataset[1].shape)
- #   model = load_model(cfg.model_dir + '\\model_%03d.h5' % (cfg.load_point))
+if cfg.traintest == 'test':
+   test_dataset = load_real_samples(cfg, train  = False)
+
+   model = load_model(cfg.pretrain_model)
+   Accuracy, Recall, L2 = Predict(cfg,model, test_dataset)
  #   ypred = model.predict([test_dataset[0],lat_input])
     
  #   save_prediction(cfg.n_testsamples, cfg.pred_dir, test_dataset[1],ypred)
